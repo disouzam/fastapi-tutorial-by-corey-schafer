a9f8a726939150205319990d4020b99d88062fb5
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-05-25 08:03:10 -0300

    Added user schemas to current list of schemas

--- schemas.py
+++ schemas.py
@@ -1,4 +1,17 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    pass
 
 
 class PostBase(BaseModel):
